12fe2373435e9336c5448b10ae86ab8972a8e021
Daniel <ndamukunda139@gmail.com>
2025-11-05 00:30:10 +0200

    basic operation file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+number1 = 10
+number2 = 5
+# Addition
+sum_result = number1 + number2
+print("Addition of ", number1, "and", number2,"is", sum_result)
+
+# Subtraction
+difference = number1 - number2
+print("Subtraction of ", number1, "and", number2,"is", difference)
+
+# Multiplication
+product = number1 * number2
+print("Multiplication of ", number1, "and", number2,"is", product)
