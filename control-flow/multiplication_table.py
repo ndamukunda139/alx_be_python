b8294d9c734751fd6b0782abfbdc9ad62203d874
ndamukunda139 <37266165+ndamukunda139@users.noreply.github.com>
2025-11-11 17:51:57 +0200

    Fix indentation in multiplication table print statement

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,5 +4,5 @@ number = int(input("Enter a number to see its multiplication table:"))
 # Generate and Print the Multiplication Table
 for num in range(1, 11):
     product = number * num
-    print(f"{number} * {num} = {product}")
+    print(f"{number} * {num} = {product}") 
 
