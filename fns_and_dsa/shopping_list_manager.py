662cd9f5e248cb69a478adf3c98fdca71207a54c
ndamukunda139 <37266165+ndamukunda139@users.noreply.github.com>
2025-11-18 16:53:40 +0200

    Add files via upload

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,37 @@
+# An empty shopping list 
+shopping_list = []
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            shopping_list.append(input("Enter item to add:"))
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            item_to_remove = input("Enter item to remove:")
+            shopping_list.remove(item_to_remove)
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            print("Shopping List:", shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
