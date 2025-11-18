1aee0210b74ff96ca4fe659c302974aedb38bf9b
ndamukunda139 <37266165+ndamukunda139@users.noreply.github.com>
2025-11-18 17:55:19 +0200

    Add files via upload

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,26 @@
+
+import datetime
+from datetime import datetime, timedelta
+
+def display_current_datetime():
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print("Current date and time: ", current_date)
+    return current_date
+
+number_of_days = int(input("Enter number of days:"))
+
+from datetime import datetime, timedelta
+def calculate_future_date():
+    
+    future_date = datetime.now() + timedelta(days=number_of_days)
+    print("Future date after : ", future_date.strftime("%Y-%m-%d"))
+
+
+
+def main():
+    display_current_datetime()
+    calculate_future_date()
+    
+
+if __name__ == "__main__":
+    main()
