33910290c51f63bc4bf0282a94f52a48f0a7f8ba
ndamukunda139 <37266165+ndamukunda139@users.noreply.github.com>
2025-11-27 12:57:46 +0200

    Update library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -23,33 +23,18 @@ class Library():
         self.__books = {}  # Private attribute   
 
     def add_book(self, book):
-        self.__books[book.title] = book
-        return "{book.title} by {book.author}"
-
+        books = self.__books.append(book)
+        return books
+    
     def check_out_book(self, title):
-        book = self.__books.get(title)
-        if book:
-            if book.check_out():
-                return f"You have checked out '{title}'."
-            else:
-                return f"'{title}' is already checked out."
-        else:
-            return f"'{title}' not found in the library." 
+        books = self.__books.remove(book for book in self.__books if book.title == title and not book._Book__is_checked_out)
+        return books
     
     def return_book(self, title):
-        book = self.__books.get(title)
-        if book:
-            if book.return_book():
-                return f"You have returned '{title}'."
-            else:
-                return f"'{title}' was not checked out."
-        else:
-            return f"'{title}' not found in the library."
+        books = self.__books.append(book for book in self.__books if book.title == title and book._Book__is_checked_out)
+        return books
     
     def list_available_books(self):
-        available_books = [title for title, book in self.__books.items() if not book._Book__is_checked_out]
+        available_books = [book.title for book in self.__books if not book._Book__is_checked_out]
         return available_books
-
-
-
-
+        
