6ae43faa7945e1f735e25c05a2a4f180d029828d
ndamukunda139 <37266165+ndamukunda139@users.noreply.github.com>
2025-11-27 12:52:30 +0200

    Update library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,41 +4,52 @@ class Book:
         self.title = title
         self.author = author
         self.__is_checked_out = False  # Private attribute
-
+    
+    def check_out(self):
+        if not self.__is_checked_out:
+            self.__is_checked_out = True
+            return True
+        return False
+    
+    def return_book(self):
+        if self.__is_checked_out:
+            self.__is_checked_out = False
+            return True
+        return False
     
 
-class Library: 
+class Library(): 
     def __init__(self):
         self.__books = {}  # Private attribute   
 
     def add_book(self, book):
         self.__books[book.title] = book
+        return "{book.title} by {book.author}"
 
     def check_out_book(self, title):
-        if title in self.__books:
-            book = self.__books[title]
-            if not book._Book__is_checked_out:  # Accessing private attribute
-                book._Book__is_checked_out = True
+        book = self.__books.get(title)
+        if book:
+            if book.check_out():
                 return f"You have checked out '{title}'."
             else:
-                return f"Sorry, '{title}' is already checked out."
+                return f"'{title}' is already checked out."
         else:
-            return f"Sorry, '{title}' is not available in the library."  
+            return f"'{title}' not found in the library." 
     
     def return_book(self, title):
-        if title in self.__books:
-            book = self.__books[title]
-            if book._Book__is_checked_out:  # Accessing private attribute
-                book._Book__is_checked_out = False
+        book = self.__books.get(title)
+        if book:
+            if book.return_book():
                 return f"You have returned '{title}'."
             else:
                 return f"'{title}' was not checked out."
         else:
-            return f"Sorry, '{title}' does not belong to this library."
+            return f"'{title}' not found in the library."
     
     def list_available_books(self):
         available_books = [title for title, book in self.__books.items() if not book._Book__is_checked_out]
         return available_books
-    
+
+
 
 
