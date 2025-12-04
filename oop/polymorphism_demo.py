98d54ef9a7f5b503693cf0fcf6fea6c712ccbacf
ndamukunda139 <37266165+ndamukunda139@users.noreply.github.com>
2025-12-04 10:00:39 +0200

    Add files via upload

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Derived classes need to implement this method.")
+    
+class Rectangle(Shape):
+    def __init__(self, leghth, width):
+        self.leghth = leghth
+        self.width = width
+    
+    def area(self):
+        return self.leghth * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def are(self):
+        return math.pi * (self.radius ** 2)
+    
