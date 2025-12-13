7e844232941285842d4cb80614f66585de017b1e
ndamukunda139 <37266165+ndamukunda139@users.noreply.github.com>
2025-12-13 20:59:22 +0200

    Add files via upload

---
+++ Intro_to_DB/alx_book_store.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+
+CREATE TABLE Authors (
+	author_id INT PRIMARY KEY,
+    author_name VARCHAR(130)
+);
+
+CREATE TABLE Books (
+	book_id INT PRIMARY KEY,
+	title VARCHAR(130),
+	author_id INT ,
+	price DOUBLE,
+	publication_date DATE,
+    CONSTRAINT 
+		FOREIGN KEY(author_id) REFERENCES Authors(author_id)
+);
+CREATE TABLE Customers (
+	customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders (
+	order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    CONSTRAINT FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+); 
+CREATE TABLE Order_Details(
+	orderdetailid INT PRIMARY KEY,
+    order_id INT, 
+    book_id INT,
+    quantity DOUBLE, 
+    CONSTRAINT FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    CONSTRAINT FOREIGN KEY(book_id) REFERENCES Books(book_id)
+)
